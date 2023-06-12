6c6ad389f4dde2eab6c1277e245779076d1c598d
ditcu-code <work.adityac@gmail.com>
2023-06-12 21:38:17 +0700

    test

--- pages/test/index.tsx
+++ pages/test/index.tsx
@@ -2,7 +2,7 @@ const Test = () => {
   return (
     <div className="bg-gray-100">
       <div className="flex flex-col divide-y max-w-7xl divide-gray-300 mx-auto py-8">
-        <p>Test Page</p>
+        <h1>Test Page</h1>
       </div>
     </div>
   );
