3d108a5752e83f3c4294435652a7087ebae1dfea
ditcu-code <work.adityac@gmail.com>
2023-06-14 18:01:21 +0700

    fix styles in home

--- app/page.tsx
+++ app/page.tsx
@@ -28,13 +28,13 @@ export default function Home() {
 				className="absolute inset-0 -z-10 animate-fade-in"
 				quantity={100}
 			/>
-			<h1 className="z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
+			<h1 className="z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-8xl lg:text-9xl whitespace-nowrap bg-clip-text ">
 				adityacahyo
 			</h1>
 
 			<div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
 			<div className="my-16 text-center animate-fade-in">
-				<h2 className="text-xs sm:text-sm text-zinc-500 px-8">
+				<h2 className="text-xs sm:text-sm text-zinc-500 px-10">
 					a software engineer specializing in frontend development for web and iOS platforms, based in Indonesia.
 				</h2>
 			</div>
