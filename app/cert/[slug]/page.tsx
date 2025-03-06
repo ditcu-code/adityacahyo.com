8bd6974e62d989cd4a41874c9b131b998299db84
ditcu-code <work.adityac@gmail.com>
2025-03-07 00:20:31 +0700

    Add certificate page and redirect functionality for ADA and Glints

---
+++ app/cert/[slug]/page.tsx
@@ -0,0 +1,27 @@
+import { notFound, redirect } from "next/navigation";
+
+type Props = {
+  params: {
+    slug: string;
+  };
+};
+
+const certificateMap: Record<string, string> = {
+  ada: "/pdf/cert-ada-adityac.pdf",
+  glints: "/images/cert-glints-adityac.png",
+};
+
+export async function generateStaticParams(): Promise<Props["params"][]> {
+  return Object.keys(certificateMap).map((slug) => ({ slug }));
+}
+
+export default async function CertificatePage({ params }: Props) {
+  const { slug } = params;
+
+  // Redirect to the corresponding certificate file or show 404 if not found
+  if (certificateMap[slug]) {
+    redirect(certificateMap[slug]);
+  } else {
+    notFound();
+  }
+}
