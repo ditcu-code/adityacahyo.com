1e24eb5cc3f70bd473ca64c1ed140123dd13deb7
ditcu-code <work.adityac@gmail.com>
2025-03-07 13:57:21 +0700

    Update redirect path for JourneyOfAndil to new PDF location

--- app/journey-of-andil/page.tsx
+++ app/journey-of-andil/page.tsx
@@ -1,5 +1,5 @@
 import { redirect } from "next/navigation";
 
 export default async function JourneyOfAndil() {
-  redirect("/pdf/andil-startupjourney.pdf");
+  redirect("/pdf/startupjourney-andil-adityac.pdf");
 }
