d3884c8f52571f7fb18ee5442a469758509b062e
ditcu-code <work.adityac@gmail.com>
2025-03-10 04:59:38 +0700

    Enhance accessibility by adding navigation role and aria-label to the main navigation

--- app/page.tsx
+++ app/page.tsx
@@ -10,15 +10,20 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center w-screen h-screen overflow-hidden bg-gradient-to-tl from-black via-zinc-600/20 to-black">
       <nav className="my-16 animate-fade-in">
-        <ul className="flex items-center justify-center gap-4">
+        <ul
+          className="flex items-center justify-center gap-4"
+          role="navigation"
+          aria-label="Main Navigation"
+        >
           {navigation.map((item) => (
-            <Link
-              key={item.href}
-              href={item.href}
-              className="text-sm duration-500 text-zinc-500 hover:text-zinc-300"
-            >
-              {item.name}
-            </Link>
+            <li key={item.href as string}>
+              <Link
+                href={item.href}
+                className="text-sm duration-500 text-zinc-500 hover:text-zinc-300"
+              >
+                {item.name}
+              </Link>
+            </li>
           ))}
         </ul>
       </nav>
