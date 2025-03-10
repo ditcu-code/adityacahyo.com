a6675f03ed4890be31c5881dd9f8bd7493cf0b00
ditcu-code <work.adityac@gmail.com>
2025-03-10 12:52:46 +0700

    Add skew protection to Next.js configuration

--- next.config.mjs
+++ next.config.mjs
@@ -2,11 +2,13 @@ import { withContentlayer } from "next-contentlayer";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
-	experimental: {
-		appDir: true,
-		mdxRs: true,
-	},
+  pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
+  experimental: {
+    appDir: true,
+    mdxRs: true,
+    useDeploymentId: true,
+    useDeploymentIdServerActions: true,
+  },
 };
 
 export default withContentlayer(nextConfig);
