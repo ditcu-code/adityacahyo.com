7b50e67c4c986881336072ed23f78fcd4aef100f
ditcu-code <work.adityac@gmail.com>
2025-10-21 07:48:09 +0700

    Update project content: modify dineinorder platform description, add kelolaternak and kohai projects, and enhance sparkq description

--- types/enums.ts
+++ types/enums.ts
@@ -1,9 +1,10 @@
 export enum Platform {
     ios = "iOS app",
+    iosAndroid = "iOS & Android app",
     ipados = "iPadOS app",
     watchos = "watchOS app",
     web = "web",
-    both = "iOS app & web"
+    both = "web & app"
 }
 
 export enum EdgeConfig {
